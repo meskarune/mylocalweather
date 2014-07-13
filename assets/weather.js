3e7f9080bf3e0602cb9d207d8037cea135bf746d
Dolores Portalatin <admin@doloresportalatin.info>
2014-07-12 21:22:41 -0400

    Update weather.js
    Added separate cache handling function and localization function, removed duplicate ajax code.

--- assets/weather.js
+++ assets/weather.js
@@ -25,15 +25,8 @@ $(function geolocation () {
 function getcoordinates(position) {
     var lat=position.coords.latitude;
     var long=position.coords.longitude;
-    var units=localStorage.getItem("Units");
-    var CurrentWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units="+units;
-    var DailyForecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat="+lat+"&lon="+long+"&units="+units+"&cnt=1";
-    if (units == "imperial") {
-        getWeather(CurrentWeatherURL, DailyForecastURL, "F", "mph")
-    }
-    else {
-        getWeather(CurrentWeatherURL, DailyForecastURL, "C", "m\/s")
-    }
+    getWeather("weather", lat, long);
+    getWeather("forecast/daily", lat, long);
 }
 function showError(error) {
     var errorMessages = {
@@ -47,51 +40,53 @@ function showError(error) {
         if (error[msg] === error.code)
             $("#weather").html(errorMessages[msg]);
 }
-var data_timestamp=Math.round(new Date().getTime() / 1000);
-function getWeather(data_url, forecast_url, temp, wind) {
+function getWeather(type, lat, long) {
+    var units=localStorage.getItem("Units");
     $.ajax ({
-        url: data_url,
+        url: "http://api.openweathermap.org/data/2.5/" + type + "?lat=" + lat + "&lon=" + long + "&units=" + units,
         type: 'GET',
         cache: false,
-        dataType: "jsonp",
+        dataType: "text",
         success: function(data) {
-            localStorage.WeatherCache = JSON.stringify(data);
+            localStorage.setItem("cache-" + type + "-" + units, data);
+            handleCache();
         },
         error: function (errorData) {
             $("#weather").html("Error retrieving current weather data :: "+ errorData.status);
         }
     });
-    $.ajax ({
-        url: forecast_url,
-        type: 'GET',
-        cache: false,
-        datatype: "jsonp",
-        success: function(data) {
-            localStorage.ForecastCache = JSON.stringify(data);
-            displayData(temp, wind);
-        },
-        error: function (errorData) {
-            $("#forecast").html("Error retrieving forecast data :: "+ errorData.status);
-        }
-    });
-    localStorage.timestamp = data_timestamp;
 };
-function displayData(temp_units, wind_units) {
-    try {
-        // If the timestamp is newer  than 30 minutes, parse data from cache
-        if ( localStorage.getItem('timestamp') > data_timestamp - 1800){
-            var data = JSON.parse(localStorage.WeatherCache);
-            var forecast = JSON.parse(localStorage.ForecastCache);
-            document.body.style.background = "url('assets/backgrounds/" +data.weather[0].icon+ ".jpg') no-repeat fixed 50% 50%";
-            document.body.style.backgroundSize = "cover";
-            $("#weather").html('<h2>' + data.name + '</h2><img class="icon" src="assets/icons/'+data.weather[0].icon+'.png"><span id="temp">'+ data.main.temp + ' </span><span id="units">&deg;'+temp_units+'</span><p id="description">'+ data.weather[0].description + '</p><p><span id="humidity">'+ data.main.humidity + '% humidity</span>&nbsp;&nbsp;&nbsp;&nbsp;'+ Math.round(data.wind.speed) + wind_units +' wind</p>');
-            $("#forecast").html('<p id="daily">Today\'s Forecast: '+forecast.list[0].weather[0].main+'</p><p>max: '+Math.round(forecast.list[0].temp.max)+'&deg;'+temp_units+' &nbsp;&nbsp;&nbsp;&nbsp;min: ' +Math.round(forecast.list[0].temp.min)+'&deg;'+temp_units+'</p>');
-        }
-        else {
-            geolocation ();
-        }
+function handleCache() {
+    var data_timestamp=Math.round(Date.now() / 1000);
+    if (localStorage.getItem("timestamp") && localStorage.getItem("timestamp") <= data_timestamp - 1800) {
+        localize();
     }
-    catch(error){
-        window.console && console.error(error);
+    else {
+        localStorage.timestamp = data_timestamp;
+        geolocation();
+    }
+}
+function localize() {
+    if (localStorage.getItem("Units") == "imperial") {
+        displayData("imperial", "F", "mph");
+    }
+    else {
+        displayData("metric", "C", "m\/s");
+    }
+}
+function displayData(system, temp_units, wind_units) {
+    try {
+        var data = JSON.parse(localStorage.getItem("cache-weather-"+ system));
+        var forecast = JSON.parse(localStorage.getItem("cache-forecast/daily"+ system));
+        document.body.style.background = "url('assets/backgrounds/" +data.weather[0].icon+ ".jpg') no-repeat fixed 50% 50%";
+        document.body.style.backgroundSize = "cover";
+        $("#weather").html('<h2>' + data.name + '</h2><img class="icon" src="assets/icons/'+data.weather[0].icon+'.png"><span id="temp">'+ data.main.temp + ' </span><span id="units">&deg;'+temp_units+'</span><p id="description">'+ data.weather[0].description + '</p><p><span id="humidity">'+ data.main.humidity + '% humidity</span>&nbsp;&nbsp;&nbsp;&nbsp;'+ Math.round(data.wind.speed) + wind_units +' wind</p>');
+        $("#forecast").html('<p id="daily">Today\'s Forecast: '+forecast.list[0].weather[0].main+'</p><p>max: '+Math.round(forecast.list[0].temp.max)+'&deg;'+temp_units+' &nbsp;&nbsp;&nbsp;&nbsp;min: ' +Math.round(forecast.list[0].temp.min)+'&deg;'+temp_units+'</p>');
+    }
+    catch(exception){
+        if (window.console) {
+            console.error(exception);
+        }
+        return;
     }
 }
