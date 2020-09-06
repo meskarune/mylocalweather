0ce1de11a909b7517b8705f285fb94a9888ba837
Dolores Portalatin <admin@doloresportalatin.info>
2020-09-07 01:18:51 +0300

    Total rewrite
    I removed the jquery dependancy and added handling for open
    weather map's API key, the app now requires that the api key is put into
    the URL as `?key=<api>` its a bit annoying but I couldn't think of
    a better way. The code is vanilla javascript and currently very messy
    but usable.

--- assets/weather.js
+++ assets/weather.js
@@ -1,98 +1,133 @@
-function SetUnits(units) {
-    localStorage.Units = units;
-    $(".active").removeClass("active");
-    $("#" +units).addClass("active");
+var weather = document.getElementById("weather");
+var forcast = document.getElementById("forecast");
+
+function setunits(unit) {
+  localStorage.setItem("Units", unit);
 }
-$(function() {
-    $("#imperial, #metric").on("click", function() {
-        SetUnits(this.id);
-        handleCache();
-    });
-    DefaultUnits ();
-});
-function DefaultUnits () {
-    var system = localStorage.getItem("Units");
-    if (system != "metric" && system != "imperial") {
-        system = window.navigator.language == "en-US" ? "imperial" : "metric";
-    }
-    SetUnits(system);
+
+function getunits() {
+  var system = localStorage.getItem("Units");
+  if (system != "metric" && system != "imperial") {
+    system = window.navigator.language == "en-US" ? "imperial" : "metric";
+    setunits(system);
+  }
+  return localStorage.getItem("Units");
 }
-function geolocation () {
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(getcoordinates,showError);
-    }
-    else {
-        $("#weather").html("Geolocation is not supported by this browser.");
-    }
+
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
 }
-function getcoordinates(position) {
-    var lat=position.coords.latitude;
-    var long=position.coords.longitude;
-    getWeather("weather", lat, long);
-    getWeather("forecast/daily", lat, long);
+
+function getUrlParam(parameter, defaultvalue){
+  var urlparameter = defaultvalue;
+  if(window.location.href.indexOf(parameter) > -1){
+    urlparameter = getUrlVars()[parameter];
+  }
+  return urlparameter;
 }
+
+var keyid = getUrlParam('key','empty');
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getcoordinates,showError); }
+  else {
+    x.innerHTML = "Geolocation is not supported by this browser."; }
+}
+
+      function getcoordinates(position) {
+        getweather(position.coords.latitude, position.coords.longitude)
+      }
+
 function showError(error) {
-    var errorMessages = {
-        PERMISSION_DENIED    : "User denied the request for geolocation.",
-        POSITION_UNAVAILABLE : "Location information is unavailable.",
-        TIMEOUT              : "The request to get user location timed out.",
-        UNKNOWN_ERROR        : "An unknown error occurred."
-    };
-    $("#weather").html(errorMessages.UNKNOWN_ERROR);
-    for (var msg in errorMessages)
-        if (error[msg] === error.code)
-            $("#weather").html(errorMessages[msg]);
+  var errorMessages = {
+    PERMISSION_DENIED    : "User denied the request for geolocation.",
+    POSITION_UNAVAILABLE : "Location information is unavailable.",
+    TIMEOUT              : "The request to get user location timed out.",
+    UNKNOWN_ERROR        : "An unknown error occurred."
+  };
+  weather.innerHTML = errorMessages.UNKNOWN_ERROR
+  for (var msg in errorMessages)
+    if (error[msg] === error.code)
+      weather.innerHTML = errorMessages[msg]
 }
-function getWeather(type, lat, long) {
-    var units=localStorage.getItem("Units");
-    $.ajax ({
-        url: "http://api.openweathermap.org/data/2.5/" + type + "?lat=" + lat + "&lon=" + long + "&units=" + units,
-        type: 'GET',
-        cache: false,
-        dataType: "text",
-        success: function(data) {
-            localStorage.setItem("cache-" + type + "-" + units, data);
-            handleCache();
-        },
-        error: function (errorData) {
-            $("#weather").html("Error retrieving current weather data :: "+ errorData.status);
-        }
-    });
-};
+
+      var getJSON = function(url, callback) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'json';
+        request.onload = function() {
+          var code = request.status
+          if (code >= 200 && code < 400) {
+            callback(request.response);
+          }
+          else {
+            callback(null);
+            console.log(code)
+          }
+        };
+        request.send();
+      };
+
+function localize (units) {
+  if (units == "imperial") {
+    displayweather(units, "F", "mph")
+  }
+  else {
+    displayweather(units, "C", "km\/h")
+  }
+}
+
 function handleCache() {
-    var data_timestamp=Math.round(Date.now() / 1000);
-    if (localStorage.getItem("timestamp") && localStorage.getItem("timestamp") <= data_timestamp - 1800) {
-        localize();
+  var data_timestamp=Math.round(Date.now() / 1000);
+  var unitsystem = getunits();
+  if (localStorage.getItem("timestamp") && localStorage.getItem("timestamp") <= data_timestamp - 1800) {
+    localize(unitsystem);
+  }
+  else {
+    localStorage.timestamp = data_timestamp;
+    getLocation();
+  }
+}
+
+function displayweather(unitsystem, temp_unit, wind_unit) {
+  var data;
+  try {
+    data = JSON.parse(localStorage.getItem("weather-" + unitsystem));
+  }
+  catch (exception) {
+    if (window.console) {
+      console.error(exception);
+    }
+    return;
+  }
+  let DomBody = document.querySelector('#content');
+  DomBody.style.background = "url('assets/backgrounds/" + data.current.weather[0].icon + ".jpg') no-repeat fixed 50% 50%";
+  DomBody.style.backgroundSize = "cover";
+  weather.innerHTML = '<h2>Current Weather</h2><img class="icon" src="assets/icons/' + data.current.weather[0].icon + '.png"><span id="temp">' + Math.round(data.current.temp) + '&deg;' + temp_unit + '</span><p id="description">' + data.current.weather[0].description + '</p><p><span id="humidity">' + data.current.humidity + '% humidity</span>&nbsp;&nbsp;&nbsp;&nbsp;' + Math.round(data.current.wind_speed) + wind_unit + ' wind</p>'
+  forcast.innerHTML = '<p id="daily">Today\'s Forecast: ' + data.daily[0].weather[0].main + '</p><p>max: ' + Math.round(data.daily[0].temp.max)  + '&deg;' + temp_unit + ' &nbsp;&nbsp;&nbsp;&nbsp;min: ' + Math.round(data.daily[0].temp.min) + '&deg;' + temp_unit + '</p>'
+}
+
+function getweather(LAT, LON) {
+  var unitsystem = getunits();
+  var APIurl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + LAT + '&lon=' + LON + '&exclude=minutely,hourly&units=' + unitsystem + '&APPID=' + keyid
+  console.log(APIurl)
+  getJSON(APIurl, function(data) {
+    if (data == null) {
+      if (keyid == "empty"){
+        forcast.innerHTML = 'You need to add an API key to the url. `example.com?key=&lt;api&gt;`' }
+      else {
+        forcast.innerHTML = 'There was a problem with the request' }
     }
     else {
-        geolocation(function() {
-            localStorage.timestamp = data_timestamp;
-            localize();
-        });
+      localStorage.setItem("weather-" + unitsystem, JSON.stringify(data));
+      localize(unitsystem);
     }
+  });
 }
-function localize() {
-    if (localStorage.getItem("Units") == "imperial") {
-        displayData("imperial", "F", "mph");
-    }
-    else {
-        displayData("metric", "C", "m\/s");
-    }
-}
-function displayData(system, temp_units, wind_units) {
-    var data, forecast;
-    try {
-        data = JSON.parse(localStorage.getItem("cache-weather-"+ system));
-        forecast = JSON.parse(localStorage.getItem("cache-forecast\/daily-"+ system));
-    } 
-    catch (exception) {
-        if (window.console) {
-            console.error(exception);
-        }
-        return;
-    }
-    document.body.style.background = "url('assets/backgrounds/" +data.weather[0].icon+ ".jpg') no-repeat fixed 50% 50%";
-    document.body.style.backgroundSize = "cover";
-    $("#weather").html('<h2>' + data.name + '</h2><img class="icon" src="assets/icons/'+data.weather[0].icon+'.png"><span id="temp">'+ data.main.temp + ' </span><span id="units">&deg;'+temp_units+'</span><p id="description">'+ data.weather[0].description + '</p><p><span id="humidity">'+ data.main.humidity + '% humidity</span>&nbsp;&nbsp;&nbsp;&nbsp;'+ Math.round(data.wind.speed) + wind_units +' wind</p>');
-    $("#forecast").html('<p id="daily">Today\'s Forecast: '+forecast.list[0].weather[0].main+'</p><p>max: '+Math.round(forecast.list[0].temp.max)+'&deg;'+temp_units+' &nbsp;&nbsp;&nbsp;&nbsp;min: ' +Math.round(forecast.list[0].temp.min)+'&deg;'+temp_units+'</p>');
-}
+
+window.onload=handleCache();
